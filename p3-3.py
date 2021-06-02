3711af8784c60a096bbf4dc0a27e2e3108933727
Bradley Caccivio <bradley@Bradleys-MacBook-Air.local>
2021-06-02 07:49:03 -0500

    old wiring

--- p3-3.py
+++ p3-3.py
@@ -28,8 +28,8 @@ _time = 0
 while _time < 1000:
     try:
         hx711 = HX711(
-                dout_pin=5,
-                pd_sck_pin=6,
+                dout_pin=27,
+                pd_sck_pin=17,
                 channel='A',
                 gain=64
         )
